0fffdd11bc6cb1faa3cf37241f0ca593a8049ddc
Patrik <pat_p3@hotmail.com>
2025-01-16 13:21:38 +0100

    Moved .html to html folder and updated server.js

--- server.js
+++ server.js
@@ -6,16 +6,14 @@ import pug from 'pug';
 const app = express();
 const port = 5080;
 
-app.set('views', './views');
+app.set('views', path.join(process.cwd(), './views'));
 app.set('view engine', 'pug');
 
-app.get('/', async (request, response) => {
-  const buf = await fs.readFile('index.html');
-  const html = buf.toString();
-  response.send(html);
-});
+app.use('/static', express.static(path.join(process.cwd(), 'static')));
 
-app.use('/static', express.static('static'));
+app.get('/', (request, response) => {
+  response.render('index');
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
