3919ea122b05f4ac263838143db7aefa4d1c8514
Patrik <pat_p3@hotmail.com>
2025-01-16 19:26:49 +0100

    Modern CSS rendering

--- server.js
+++ server.js
@@ -2,25 +2,22 @@ import express from 'express';
 import path from 'path';
 import sass from 'sass';
 import fs from 'fs/promises';
-import pug from 'pug';
 
 const app = express();
 const port = 5080;
 
-app.use('/static/styles', (request, response, next) => {
+app.use('/static/styles', async (request, response, next) => {
   const scssPath = path.join(process.cwd(), 'static', 'styles', 'styles.scss');
   const cssPath = path.join(process.cwd(), 'static', 'styles', 'styles.css');
 
-  sass.render({ file: scssPath }, (err, result) => {
-    if (err) {
-      console.error(err);
-      return next(err);
-    }
-
-    fs.writeFile(cssPath, result.css)
-      .then(() => response.sendFile(cssPath))
-      .catch(next);
-  });
+  try {
+    const result = await sass.compileAsync(scssPath);
+    await fs.writeFile(cssPath, result.css);
+    response.sendFile(cssPath);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 app.use('/static', express.static(path.join(process.cwd(), 'static')));
