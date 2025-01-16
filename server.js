70860d185529f208686414ca0df164ba941df445
Patrik <pat_p3@hotmail.com>
2025-01-16 16:48:57 +0100

    Added sass to server config

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 import express from 'express';
 import path from 'path';
+import sass from 'sass';
 import fs from 'fs/promises';
 import pug from 'pug';
 
 const app = express();
 const port = 5080;
 
-app.set('views', path.join(process.cwd(), './views'));
-app.set('view engine', 'pug');
+app.use('/static/styles', (request, response, next) => {
+  const scssPath = path.join(process.cwd(), 'static', 'styles', 'styles.scss');
+  const cssPath = path.join(process.cwd(), 'static', 'styles', 'styles.css');
+
+  // Kompilera SCSS till CSS
+  sass.render({ file: scssPath }, (err, result) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+
+    // Skriv den genererade CSS-filen
+    fs.writeFile(cssPath, result.css)
+      .then(() => response.sendFile(cssPath))
+      .catch(next);
+  });
+});
 
 app.use('/static', express.static(path.join(process.cwd(), 'static')));
+
+app.set('views', path.join(process.cwd(), './views'));
+app.set('view engine', 'pug');
 
 app.get('/', (request, response) => {
   response.render('index');
