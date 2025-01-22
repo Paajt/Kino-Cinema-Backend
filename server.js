71170baac7934daa5b6e3630c0a87a4f1e189a2e
Patrik <pat_p3@hotmail.com>
2025-01-22 14:20:22 +0100

    SoC Added/removed code for jest testing

--- server.js
+++ server.js
@@ -1,55 +1,5 @@
-import express from 'express';
-import path from 'path';
-import * as sass from 'sass';
-import fs from 'fs/promises';
-import { loadAllMovies, loadSingleMovie } from './static/js/movies.js';
+import app from './static/js/app.js';
 
-const app = express();
-const port = 5080;
-
-app.use('/static/styles', async (request, response, next) => {
-  const scssPath = path.join(process.cwd(), 'static', 'styles', 'styles.scss');
-  const cssPath = path.join(process.cwd(), 'static', 'styles', 'styles.css');
-
-  try {
-    const result = await sass.compileAsync(scssPath);
-    await fs.writeFile(cssPath, result.css);
-    response.sendFile(cssPath);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-app.use('/static', express.static(path.join(process.cwd(), 'static')));
-
-app.set('views', path.join(process.cwd(), './views'));
-app.set('view engine', 'pug');
-
-app.get('/', (request, response) => {
-  response.render('index', { page: 'home' });
-});
-
-app.get('/about', (request, response) => {
-  response.render('about');
-});
-
-app.get('/movies', async (request, response) => {
-  const allMovies = await loadAllMovies();
-  response.render('movies', { allMovies });
-});
-
-app.get('/movies/:movieId', async (request, response) => {
-  const movieId = request.params.movieId;
-  const singleMovie = await loadSingleMovie(movieId);
-
-  if (!singleMovie) {
-    return response.status(404).render('404');
-  }
-
-  response.render('movieID', { singleMovie });
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(5080, () => {
+  console.log(`Server running at http://localhost:5080`);
 });
