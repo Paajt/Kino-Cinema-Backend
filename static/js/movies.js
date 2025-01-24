9d71cfa57464369a6fbc00ba201023438d56e681
Patrik <pat_p3@hotmail.com>
2025-01-24 11:01:33 +0100

    added try/catch to functions

--- static/js/movies.js
+++ static/js/movies.js
@@ -4,26 +4,42 @@ import { marked } from 'marked';
 const Movie_API = 'https://plankton-app-xhkom.ondigitalocean.app/api/';
 
 export async function loadAllMovies() {
-  const response = await fetch(Movie_API + 'movies');
-  const payload = await response.json();
-  return payload.data.map((movie) => ({
-    id: movie.id,
-    ...movie.attributes,
-  }));
+  try {
+    const response = await fetch(Movie_API + 'movies');
+
+    if (!response.ok) {
+      console.error('Failed to fetch movies:', response.statusText);
+      return [];
+    }
+
+    const payload = await response.json();
+    return payload.data.map((movie) => ({
+      id: movie.id,
+      ...movie.attributes,
+    }));
+  } catch (error) {
+    console.error('Error fetching all movies', error.message);
+    return [];
+  }
 }
 
 export async function loadSingleMovie(id) {
-  const response = await fetch(`${Movie_API}movies/${id}`);
+  try {
+    const response = await fetch(`${Movie_API}movies/${id}`);
 
-  if (!response.ok) {
+    if (!response.ok) {
+      return null;
+    }
+
+    const payload = await response.json();
+    return {
+      id: payload.data.id,
+      title: payload.data.attributes.title,
+      intro: marked(payload.data.attributes.intro),
+      image: payload.data.attributes.image?.url,
+    };
+  } catch (error) {
+    console.error(`Error fetching movie with ID ${id}:`, error.message);
     return null;
   }
-
-  const payload = await response.json();
-  return {
-    id: payload.data.id,
-    title: payload.data.attributes.title,
-    intro: marked(payload.data.attributes.intro),
-    image: payload.data.attributes.image?.url,
-  };
 }
